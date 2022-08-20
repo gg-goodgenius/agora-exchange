7df4aca93246debce95c147bfe8cea12943ad859
“Timofey121” <ale3jurtaev@gmail.com>
2022-08-20 21:16:55 +0400

    yml parsing

--- scripts/yml/parsing_yml.py
+++ scripts/yml/parsing_yml.py
@@ -1,4 +1,9 @@
+import datetime
+import json
+
 import ruamel.yaml
+
+start_time = datetime.datetime.now()
 
 file = f'case_2_input_data.yml'
 
@@ -6,30 +11,5 @@ yaml = ruamel.yaml.YAML(typ='safe')
 with open(file, 'r') as file:
     data = yaml.load(file)
 
-for item in data[0]['ГруппаНоменклатура']:
-    url = item['Ссылка']
-    parent = item['Родитель']
-    name = item['Наименование']
-    tag = item['ПометкаУдаления']
-
-for item in data[1]['ЕдиницыИзмерения']:
-    units_of_measurement = item['ЕдиницыИзмерения']
-    url = item['Ссылка']
-    full_name = item['НаименованиеПолное']
-    coefficient = item['Коэффициент']
-
-for item in data[2]['Номенклатура']:
-    url = item['Ссылка']
-    parent = item['Родитель']
-    tag = item['ПометкаУдаления']
-    article = item['Артикул']
-    name = item['Наименование']
-    nds = item['СтавкаНДС']
-    description = item['Описание']
-    storage_unit = item['ЕдиницаХраненияОстатков']
-    characteristic = item['ВестиУчетПоХарактеристикам']
-    type = item['ТипНоменклатуры']
-    barcode = item['Штрихкод']
-    file = item['Файл']
-    path = item['Путь']
-    general = item['Основное']
+print(datetime.datetime.now() - start_time)
+print(json.dumps(data, ensure_ascii=False, indent=4))
