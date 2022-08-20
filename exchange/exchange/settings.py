c6f6802c8121d04230c3be36238e1287e31c2a47
Ruslan Khairullin <ruslan.gzn@gmail.com>
2022-08-20 18:07:42 +0500

    update settings.py

--- exchange/exchange/settings.py
+++ exchange/exchange/settings.py
@@ -137,3 +137,13 @@ CELERY_TASK_TRACK_STARTED = True
 CELERY_TASK_TIME_LIMIT = 30 * 60
 CELERY_BROKER_URL = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
 CELERY_RESULT_BACKEND = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
+
+SWAGGER_SETTINGS = {
+   'SECURITY_DEFINITIONS': {
+      'Token': {
+            'type': 'apiKey',
+            'name': 'Authorization',
+            'in': 'header'
+      }
+   }
+}
