09c66a311fc59d8cc7a0745db3a807fa2219a1ff
Ruslan Khairullin <ruslan.gzn@gmail.com>
2022-08-20 16:05:22 +0500

    Added nginx

--- exchange/core/views.py
+++ exchange/core/views.py
@@ -1,7 +1,1 @@
 from django.shortcuts import render
-from django.views.generic import ListView
-from django.http import HttpResponse
-from core.models import Product
-
-class MainView(ListView):
-    model = Product
--- exchange/erpexch/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ErpexchConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'erpexch'
